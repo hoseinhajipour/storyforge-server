504faf23bf3c31d55d2a5310e992935610a8de86
hajipour <hoseinhajepor@gmail.com>
2023-10-31 22:22:00 +0330

    add characters

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Scene;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/scenes', function () {
+    return Scene::all();
+  });
+  
+  Route::get('/characters', function () {
+    return Character::all();
+  });
+
+  
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
